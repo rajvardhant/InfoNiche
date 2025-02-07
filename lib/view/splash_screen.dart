53b31b306b3d9e3ad8491590de816cab16caf3c0
rajvardhan1 <raj.vardhan305844@gmail.com>
2025-02-07 15:31:20 +0530

    Change App Icon

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -34,7 +34,7 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment:MainAxisAlignment.center ,
           children: [
             Image.asset(
-              'images/news1.png',
+              'assets/images/news1.png',
               fit: BoxFit.cover,
               width: widht * 1,
               height: height *.5,
